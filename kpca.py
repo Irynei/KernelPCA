ff52a5d636d1c31cad0f21d257e1bd81d2d94590
Irynei Baran <i.baran@ucu.edu.ua>
2018-02-12 17:30:35 +0200

    Refactor. Add docstrings.

--- kpca.py
+++ kpca.py
@@ -7,8 +7,8 @@ from sklearn.metrics import pairwise_kernels
 
 class Kernel:
 
-    def __init__(self, kernel_name, gamma, degree):
-        self.kernel_name = kernel_name
+    def __init__(self, kernel_type, gamma, degree):
+        self.kernel_name = kernel_type
         self.gamma = gamma
         self.degree = degree
         self.kernels = {
@@ -18,10 +18,18 @@ class Kernel:
         }
 
     def get_kernel(self, x):
+        """
+        Return kernel matrix computed on x using specific kernel.
+        Available kernel types: rbf, linear, poly
+        """
         return self.kernels.get(self.kernel_name)(x)
 
     def rbf_kernel(self, x):
-        # Gaussian kernel
+        """
+        Gaussian kernel
+        :param x: data
+        :return kernel matrix
+        """
         sq_dists = pdist(x, 'sqeuclidean')
         mat_sq_dists = squareform(sq_dists)
         kernel = -self.gamma * mat_sq_dists
@@ -29,25 +37,36 @@ class Kernel:
         return kernel
 
     def poly_kernel(self, x):
+        """
+        Polynomial kernel
+        :param x: data
+        :return: kernel matrix
+        """
         return matrix_power(x.dot(x.T) + self.gamma, self.degree)
 
     def linear_kernel(self, x):
+        """
+        Linear kernel
+        :param x: data
+        :return kernel_matrix
+        """
         return x.dot(x.T)
 
 
 class KernelPCA:
 
     def __init__(self, kernel='rbf', gamma=10, degree=3, n_components=2):
-        self.kernel = Kernel(kernel_name=kernel, gamma=gamma, degree=degree)
+        self.kernel = Kernel(kernel_type=kernel, gamma=gamma, degree=degree)
         self.gamma = gamma
         self.degree = degree
+        self.fitted = None
         self.kernel_matrix = None
         self.eigenvalues = None
         self.eigenvectors = None
-        self.fitted = None
         self.n_components = n_components
 
     def _center_kernel(self):
+        """ Centralizing kernel matrix. """
         n = self.kernel_matrix.shape[0]
         one_n = np.ones((n, n)) / n
         self.kernel_matrix -= one_n.dot(self.kernel_matrix)
@@ -55,6 +74,7 @@ class KernelPCA:
         self.kernel_matrix += one_n.dot(self.kernel_matrix).dot(one_n)
 
     def _kernel_centerer_from_scikit_learn(self):
+        """ Kernel centerer taken from scikit-learn lib. """
         n_samples = self.kernel_matrix.shape[0]
         self.K_fit_rows_ = np.sum(self.kernel_matrix, axis=0) / n_samples
         self.K_fit_all_ = self.K_fit_rows_.sum() / n_samples
@@ -64,25 +84,32 @@ class KernelPCA:
         self.kernel_matrix -= K_pred_cols
         self.kernel_matrix += self.K_fit_all_
 
-    def transform(self, x):
-        if self.fitted is None:
-            self.kernel_matrix = self.kernel.get_kernel(x)
-            self._center_kernel()
-        else:
-            # used in contour plotting
-            self.kernel_matrix = pairwise_kernels(self.fitted, x, metric='rbf', gamma=self.gamma).T
-            self._kernel_centerer_from_scikit_learn()
+    def get_projection(self, x):
+        """ Compute projection of `x` onto the first `self.n_components` principal components. """
+        self.kernel_matrix = self.kernel.get_kernel(x)
+        self._center_kernel()
 
         # find eigenvectors and eigenvalues of Kernel matrix
-        if self.eigenvectors is None:
-            self.eigenvalues, self.eigenvectors = eigh(
-                self.kernel_matrix,
-                eigvals=(self.kernel_matrix.shape[0] - self.n_components, self.kernel_matrix.shape[0] - 1)
-            )
-            # sort in descending order
-            indices = self.eigenvalues.argsort()[::-1]
-            self.eigenvalues = self.eigenvalues[indices]
-            self.eigenvectors = self.eigenvectors[:, indices]
-            self.fitted = x
+        self.eigenvalues, self.eigenvectors = eigh(
+            self.kernel_matrix,
+            eigvals=(self.kernel_matrix.shape[0] - self.n_components, self.kernel_matrix.shape[0] - 1)
+        )
+        # sort in descending order
+        indices = self.eigenvalues.argsort()[::-1]
+        self.eigenvalues = self.eigenvalues[indices]
+        self.eigenvectors = self.eigenvectors[:, indices]
+        self.fitted = x
+        print("Computed eigenvalues: {}".format(self.eigenvalues))
         # return projection
+        return np.dot(self.kernel_matrix, self.eigenvectors / np.sqrt(self.eigenvalues))
+
+    def get_contour(self, x):
+        """
+        Compute contours. Compute pairwise kernel matrix from already fitted and new x.
+        Project new data `x` onto existing principal components.
+        """
+        if self.fitted is None:
+            raise Exception("KPCA is not fitted")
+        self.kernel_matrix = pairwise_kernels(self.fitted, x, metric=self.kernel.kernel_name, gamma=self.gamma).T
+        self._kernel_centerer_from_scikit_learn()
         return np.dot(self.kernel_matrix, self.eigenvectors / np.sqrt(self.eigenvalues))
--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import matplotlib.pyplot as plt
 from kpca import KernelPCA
 
 np.random.seed(0)
-X, y = make_moons(n_samples=100, noise=.05)
-# X, y = make_circles(n_samples=400, factor=.3, noise=.05)
+X_1, y_1 = make_circles(n_samples=400, factor=.3, noise=.05)
+X_2, y_2 = make_moons(n_samples=100, noise=.05)
 
-kpca = KernelPCA(kernel='rbf', gamma=10)
-X_kpca = kpca.transform(X)
+kpca_1 = KernelPCA(kernel='rbf', gamma=10, n_components=2)
+X_kpca_1 = kpca_1.get_projection(X_1)
+
+kpca_2 = KernelPCA(kernel='rbf', gamma=10, n_components=2)
+X_kpca_2 = kpca_2.get_projection(X_2)
 
 
 def scatter_plot(X, reds, blues, x_label, y_label, title):
@@ -19,23 +22,34 @@ def scatter_plot(X, reds, blues, x_label, y_label, title):
     plt.ylabel(y_label)
 
 
-def plot_contour():
+def plot_contour(kpca):
     X1, X2 = np.meshgrid(np.linspace(-1.5, 1.5, 50), np.linspace(-1.5, 1.5, 50))
     X_grid = np.array([np.ravel(X1), np.ravel(X2)]).T
-    Z_grid = kpca.transform(X_grid)[:, 0].reshape(X1.shape)
+    Z_grid = kpca.get_contour(X_grid)[:, 0].reshape(X1.shape)
     plt.contour(X1, X2, Z_grid, colors='grey', linewidths=1, origin='lower')
 
-
 if __name__ == '__main__':
+
     plt.figure()
     plt.subplot(2, 2, 1, aspect='equal')
     # plot original space with contour
-    scatter_plot(X, y == 0, y == 1, x_label='$x_1$', y_label='$x_1$', title='Original space')
-    plot_contour()
+    scatter_plot(X_1, y_1 == 0, y_1 == 1, x_label='$x_1$', y_label='$x_1$', title='Original space')
+    plot_contour(kpca_1)
 
     # plot kpca projection
     plt.subplot(2, 2, 2, aspect='equal')
-    scatter_plot(X_kpca, y == 0, y == 1,
+    scatter_plot(X_kpca_1, y_1 == 0, y_1 == 1,
+                 x_label='1st principal component in space induced by $\phi$',
+                 y_label='2nd component', title='2nd component')
+
+    plt.subplot(2, 2, 3, aspect='equal')
+    # plot original space with contour
+    scatter_plot(X_2, y_2 == 0, y_2 == 1, x_label='$x_1$', y_label='$x_1$', title='Original space')
+    plot_contour(kpca_2)
+
+    # plot kpca projection
+    plt.subplot(2, 2, 4, aspect='equal')
+    scatter_plot(X_kpca_2, y_2 == 0, y_2 == 1,
                  x_label='1st principal component in space induced by $\phi$',
                  y_label='2nd component', title='2nd component')
 
